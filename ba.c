a7d1b422fda8aac4651e241e3c942baf3475fd16
anuragsinha4345 <48663212+anuragsinha4345@users.noreply.github.com>
2019-03-28 07:57:24 +0530

    if else
    simple function grade system in c language (code block compiler)

---
+++ ba.c
@@ -0,0 +1,33 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+
+if(a<=90)
+{
+    printf("grade is A+");
+
+if(a<=80)
+{
+  printf("equal");
+
+if(a<=70)
+{
+    printf("grade is B");
+
+if(a<=60)
+{
+    printf("grade is c");
+
+}
+  }
+  }
+  }
+  else
+  printf("not equal");//
+  getch();
+  return 0;
+}
