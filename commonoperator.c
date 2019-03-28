44dca2cbc52156779f9527cec660b816c78e090d
anuragsinha4345 <48663212+anuragsinha4345@users.noreply.github.com>
2019-03-28 08:05:31 +0530

    logical operator
    (AND operator ) , (OR operator),( NOT operator) if else statement in c language in code block.

---
+++ commonoperator.c
@@ -0,0 +1,36 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int g=190,garde=90;
+    //common
+    printf("%s \n", grade>=50? "Passed": "failed");
+    //and
+    if( (grade>=65) && (grade<=95))
+    {
+        printf("pass");
+    }
+    else
+    {
+        printf("fail");
+    }
+    // or operator
+    if( (grade>=33)||(grade>=23))
+    {
+        printf("pass");
+    }
+    else
+    {
+        printf("fail");
+    }
+    //not
+    if(!g==10))
+    {
+        printf("if block");
+    }
+    else
+    {
+        printf("else block");
+    }
+getch();
+}
